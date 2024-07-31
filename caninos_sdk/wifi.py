09185e77a363f714e16c73a83151b83177194df4
Christian Bueno <christianoliveira1908@hotmail.com>
2024-07-31 14:51:59 -0300

    wifi: added network_name, network_type and interface in ConnectionInfo

--- caninos_sdk/wifi.py
+++ caninos_sdk/wifi.py
@@ -1,8 +1,53 @@
 import socket
+import subprocess
 
 class ConnectionInfo:
     CONNECTED = "connected"
     DISCONNECTED = "disconnected"
+
+
+    def __init__(self):
+        self.network_name = None
+        self.interface = None
+        self.network_type = None        
+
+        info = self.__get_connected_network_info()
+
+        self.network_name = info['network_name']
+        self.interface = info['interface']
+        self.network_type = info['network_type']        
+
+    def __get_connected_network_info(self):
+        ''' run the nmcli command to get the network_name, interface and network_type '''
+        
+        result = subprocess.run(['nmcli', 'connection', 'show', '--active'], stdout=subprocess.PIPE, text=True)
+
+        output = result.stdout
+
+        lines = output.splitlines()
+
+        # if is disconnected, return None for network_name, interface and network_type.
+        if len(lines) <= 1:
+            return {
+                'network_name': None,
+                'interface': None,
+                'network_type': None            
+                }
+
+        # split the nmcli command result.
+        # index 1 contains the values. index 0 contains the headers
+        line_infos = lines[1].split()
+        
+        network_name = line_infos[0]
+        network_type = line_infos[2]
+        interface = line_infos[3]
+
+        return {
+            'network_name': network_name,
+            'interface': interface,
+            'network_type': network_type            
+        }        
+        
 
     @property
     def status(self):
@@ -15,7 +60,9 @@ class ConnectionInfo:
         if self.__has_internet_connection():            
             return self.CONNECTED
         
-        return self.DISCONNECTED           
+        return self.DISCONNECTED
+   
+        
 
     @property
     def ip_address(self):
--- examples/wifi_example.py
+++ examples/wifi_example.py
@@ -5,3 +5,5 @@ labrador = k9.Labrador()
 
 print(labrador.wifi.status)
 print(labrador.wifi.ip_address)
+print(labrador.wifi.network_name)
+print(labrador.wifi.interface)
